ba16c91e86cc7a206757505bb5968f78fb702f76
pragatisharma2 <pragati.sharma@appfoster.com>
2025-01-14 13:11:49 +0530

    first commit

---
+++ SQL-10/abc.sql
@@ -0,0 +1,10 @@
+  INSERT INTO Customers(first_name,last_name,age,country)
+  VALUES('John','Doe',31,'USA');
+  VALUES('Robert','Luna',22,'USA');
+  VALUES('David','Robinson',22,'UK');
+  VALUES('John','Reinhardt',25,'UK');
+  VALUES('Betty','Doe',31,'UAE');
+
+  /*
+  
+  */
